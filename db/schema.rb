6d76a3aaca1447957dfce290b2010a909120d515
MargaritaKhodzka <>
2021-09-05 14:06:24 -0400

    added m_rates table to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_180220) do
+ActiveRecord::Schema.define(version: 2021_09_05_180326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "m_rates", force: :cascade do |t|
+    t.integer "wine_id"
+    t.float "grade"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "n_rates", force: :cascade do |t|
     t.integer "wine_id"

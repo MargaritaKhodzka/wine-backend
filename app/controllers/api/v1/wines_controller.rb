fd1e78194d3d959e6f0568a33319d6cb0ad76ef5
MargaritaKhodzka <>
2021-09-14 23:00:34 -0400

    set up wine controller

--- app/controllers/api/v1/wines_controller.rb
+++ app/controllers/api/v1/wines_controller.rb
@@ -1,6 +1,31 @@
 class Api::V1::WinesController < ApplicationController
   def index
     @wines = Wine.all
-    render json: @wines, status: 200
+    render json: @wines
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    if @wine.save
+      render json: @wine
+    else
+      render json: {error: 'Error creating wine'}
+    end
+  end
+
+  def show
+    @wine = Wine.find(params[:id])
+    render json: @wine
+  end
+
+  def destroy
+    @wine = Wine.find(params[:id])
+    @wine.destroy
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:date, :name, :description, :year, :shop, :price)
   end
 end

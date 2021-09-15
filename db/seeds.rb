a69e3074888c35a460fb5e04b4f77e6bac608e0b
MargaritaKhodzka <>
2021-09-14 22:38:33 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,78 @@ wine_list = Wine.create([
     shop: 'Amsterdam Wine Co',
     price: 25.00
   },
+
+  {
+    date: '2021-08-12',
+    name: 'Ancarani - Andataeritorno',
+    description: 'Orange - Skin Contact',
+    year: 2020,
+    shop: 'Amsterdam Wine Co',
+    price: 20.00
+  },
+
+  {
+    date: '2021-08-13',
+    name: 'Benito Santos - Igrexario de Saiar Albarino',
+    description: 'White',
+    year: 2020,
+    shop: 'Amsterdam Wine Co',
+    price: 17.00
+  },
+
+  {
+    date: '2021-08-15',
+    name: 'Villa Job - Untitled',
+    description: 'White - Light Skin Contact',
+    year: 2018,
+    shop: 'Nolita Wine',
+    price: 30.00
+  },
+
+  {
+    date: '2021-08-19',
+    name: 'Chateau de la Ragotiere - Les Vielles Vignes',
+    description: 'White - Mucadet',
+    year: 2018,
+    shop: 'Seattle',
+    price: 19.00
+  },
+
+  {
+    date: '2021-08-21',
+    name: 'Villa Job - Risic Blanc',
+    description: 'White - Light Skin Contact',
+    year: 2017,
+    shop: 'Seattle',
+    price: 34.00
+  },
+
+  {
+    date: '2021-08-23',
+    name: 'Pajzos - Tokaj - Furmint',
+    description: 'White - Dry',
+    year: 2019,
+    shop: 'Seattle',
+    price: 17.00
+  },
+
+  {
+    date: '2021-08-27',
+    name: 'Nando - Jakot',
+    description: 'Orange - Skin Contact',
+    year: 2018,
+    shop: 'Discovery Wine',
+    price: 26.00
+  },
+
+  {
+    date: '2021-08-29',
+    name: 'Tenuta Terre Nere - Etna Bianco - Calderara Sottana',
+    description: 'White',
+    year: 2019,
+    shop: 'Enoteca LIC',
+    price: 45.00
+  }
 ])
 
 m_rates = MRate.create([
@@ -49,6 +121,46 @@ m_rates = MRate.create([
   {
     grade: 9,
     wine_id: 3
+  },
+
+  {
+    grade: 6.5,
+    wine_id: 4
+  },
+
+  {
+    grade: 9,
+    wine_id: 5
+  },
+
+  {
+    grade: 7,
+    wine_id: 6
+  },
+
+  {
+    grade: 5,
+    wine_id: 7
+  },
+
+  {
+    grade: 9.5,
+    wine_id: 8
+  },
+
+  {
+    grade: 9,
+    wine_id: 9
+  },
+
+  {
+    grade: 5,
+    wine_id: 10
+  },
+
+  {
+    grade: 10,
+    wine_id: 11
   }
 ])
 
@@ -66,5 +178,45 @@ n_rates = NRate.create([
   {
     grade: 6.5,
     wine_id: 3
+  },
+
+  {
+    grade: 7,
+    wine_id: 4
+  },
+
+  {
+    grade: 7.5,
+    wine_id: 5
+  },
+
+  {
+    grade: 7.5,
+    wine_id: 6
+  },
+
+  {
+    grade: 5,
+    wine_id: 7
+  },
+
+  {
+    grade: 8,
+    wine_id: 8
+  },
+
+  {
+    grade: 6.5,
+    wine_id: 9
+  },
+
+  {
+    grade: 8,
+    wine_id: 10
+  },
+
+  {
+    grade: 8.5,
+    wine_id: 11
   }
 ])

c236d1762e9467d40865427637f3f9afc965e435
MargaritaKhodzka <>
2021-09-14 23:32:10 -0400

    set up m_rates and n_rates controllers

--- app/controllers/api/v1/n_rates_controller.rb
+++ app/controllers/api/v1/n_rates_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::NRatesController < ApplicationController
+
+  before_action :set_wine
+
+  def show
+    @n_rate = @wine.n_rates.find_by(id: params[:id])
+    render json: @n_rate
+  end
+
+  def create
+    @n_rate = @wine.n_rates.new(nrate_params)
+    if @n_rate.save
+      render json: @n_rate
+    else
+      render json: {error: 'Error creating grade'}
+    end
+  end
+
+  def destroy
+    @n_rate = @wine.n_rates.find_by(id: params[:id])
+    @n_rate.destroy
+  end
+
+  private
+
+  def set_wine
+    @wine = Wine.find(params[:wine_id])
+  end
+
+  def nrate_params
+    params.require(:n_rate).permit(:wine_id, :grade)
+  end
+
 end
